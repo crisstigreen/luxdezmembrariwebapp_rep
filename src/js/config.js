51087cfe309663d3a922b539ca8b4aed00971631
cristiancercelwiz <161705388+cristiancercelwiz@users.noreply.github.com>
2024-12-17 18:26:39 +0200

    config
    config

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,12 @@
-const API_BASE_URL = 'https://luxdezmembrariapi20241205192447.azurewebsites.net/api';					  
+				  
 //const API_BASE_URL = 'https://localhost:7216/api';
 
-const API_BASE_URL_IMG = 'https://localhost:7216';
-const BASE_URL = 'http://localhost:8080/';
+//const API_BASE_URL_IMG = 'https://localhost:7216';
+//const BASE_URL = 'http://localhost:8080/';
+
+
+
+const API_BASE_URL = 'https://luxdezmembrariapi20241205192447.azurewebsites.net/api';	
+const API_BASE_URL_IMG = 'https://luxdezmembrariapi20241205192447.azurewebsites.net/';
+const BASE_URL = 'https://luxdezmembrariapi20241205192447.azurewebsites.net/';
+
